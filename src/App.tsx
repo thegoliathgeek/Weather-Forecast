f73211b59205c5df4d5de29a8f1fc8db6e636585
Dhanush <dhanushdazz@gmail.com>
2019-09-02 10:56:19 +0530

    Format date

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import{BrowserRouter as Router , Route} from 'react-router-dom';
 import Home from "./Home/Home";
 
 const App: React.FC = () => {
+    const [months1,update] = useState([
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ]);
   return (
    <Router>
-     <Route exact path='/' component={Home}/>
+       <Route exact path='/' months={months1} component={()=> <Home months={months1}> </Home>}/>
    </Router>
   );
 };
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,19 +1,16 @@
 import React, {Component} from "react";
 
-interface State {
-    data: {}
-}
-
-const convertToDate: any = (epoch: number) => {
+const convertToDate: any = (epoch: number,months: any) => {
     const d = new Date(0);
     d.setUTCSeconds(epoch);
-    return d.toTimeString();
+    console.log(d.getMonth());
+    return d.getDate().toString()+" "+months[d.getMonth()]+" "+d.getFullYear();
 };
 
 const callApi: any = (ob: any) => {
     return fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ded211a3d6cf32db932883277e961ab4/13.339168,77.113998').then(res => {
         return res.json().then(jsn => {
-            console.log(jsn);
+            // console.log(jsn);
             // @ts-ignore
             ob.setState({data: jsn});
         }).catch(e => {
@@ -24,12 +21,17 @@ const callApi: any = (ob: any) => {
         console.log('Fetch Error : ' + err.message);
     });
 };
+interface Mon {
+    months: any
+}
 
-export default class Home extends Component {
+export default class Home extends Component<{months:any}> {
     constructor(props: any) {
         super(props);
+        console.log("Months : "+this.props.months);
         callApi(this);
     }
+    public static propTypes = {};
 
     state = {
         data: {
@@ -75,16 +77,18 @@ export default class Home extends Component {
                 {
                     this.state.data ?
                         <div>
-                            <h2 className="border-2">Current</h2>
-                            <p>Time : {convertToDate(this.state.data.currently.time)}</p>
+                            <div>
+                            <h2 style={{'textAlign':'center'}} className="alert alert-primary">Current</h2>
+                            </div>
+                                <p>Time : {convertToDate(this.state.data.currently.time,this.props.months)}</p>
                             <p>{this.state.data.latitude}</p>
                             <p>{this.state.data.longitude}</p>
-                            <h2>Hourly</h2>
+                            <h2 style={{'textAlign':'center'}} className="alert alert-primary">Hourly</h2>
                             {
                                 this.state.data.hourly.data.map((val: any, key1) => {
                                     return <div key={key1}>
 
-                                        <p>{convertToDate(val.time)}</p>
+                                        <p>{convertToDate(val.time,this.props.months)}</p>
                                         <p>Humidity: {val.humidity}</p>
                                     </div>
                                 })
@@ -95,4 +99,5 @@ export default class Home extends Component {
             </div>
         </div>);
     }
+
 }
