a8e8b8a870274b81f894e87c0de246741a1ca728
Dhanush <dhanushdazz@gmail.com>
2019-09-06 00:39:28 +0530

    getArea name

--- src/Views/View.tsx
+++ src/Views/View.tsx
@@ -1,10 +1,31 @@
 import React, {Component} from "react";
 import './View.scss';
 
+// 602dc18b5b6c4ca2928e944b4d35fd6f
+// https://api.opencagedata.com/geocode/v1/json?q=13.339168+77.113998&key=602dc18b5b6c4ca2928e944b4d35fd6f
+
+//TODO Fix getNames
+const getNames: any = (lat: any="13.339168", long: any="77.113998", ob: any) => {
+     return fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${long}&key=602dc18b5b6c4ca2928e944b4d35fd6f`,{
+         method: 'GET'
+     }).then((e) => {
+         return e.json().then(rt => {
+            console.log(rt.results[0].components.county);
+            ob.setState({name: rt.results[0].components.county});
+            return {name: rt.results[0].components.county};
+        }).catch(eee => {
+            console.log("Json Error");
+            console.log(eee.message);
+        })
+    }).catch((err) => {
+        console.log("Fetch Error");
+        console.log(err.message);
+    });
+};
+
 const callApi: any = (ob: any) => {
     return fetch('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/ded211a3d6cf32db932883277e961ab4/13.339168,77.113998').then(res => {
         return res.json().then(jsn => {
-            // console.log(jsn);
             // @ts-ignore
             ob.setState({data: jsn});
         }).catch(e => {
@@ -16,12 +37,19 @@ const callApi: any = (ob: any) => {
     });
 };
 
+
 export default class View extends Component<{ months: Array<string>, prevData: any }> {
     constructor(props: any) {
         super(props);
-        callApi(this);
     }
-    state= {
+    componentDidMount(): void {
+        callApi(this);
+
+        getNames(this);
+    }
+
+    state = {
+        name:"",
         data: {
             latitude: '',
             longitude: '',
@@ -62,11 +90,12 @@ export default class View extends Component<{ months: Array<string>, prevData: a
     render() {
         return (<div>
             <div className='containerMine'>
-              <div className='weather-container'>
+                <div className='weather-container'>
                     <div className='data-cont'>
-                        <p>{this.state.data.currently.temperature}</p>
+                        <p>Current temperature: <span>{this.state.data.currently.temperature} &#x2109;</span></p>
+                        <p>{this.state.name}</p>
                     </div>
-              </div>
+                </div>
             </div>
         </div>);
     }
